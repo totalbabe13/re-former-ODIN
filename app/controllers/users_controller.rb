7ca70a98072745aee22042b88cc84de0a6d36d6a
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-03-19 17:54:42 -0500

    indentation correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,20 @@
 class UsersController < ApplicationController
-	def new
+  def new
+  end	
 
-	end	
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password_digest: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+   	  redirect_to new_users_path
+  	else
+      render :new
+  	end
+  end
 
-	def create
-    	# @user = User.new(username: params[:username], email: params[:email], password_digest: params[:password])
-    	  @user = User.new(user_params)
-  		if @user.save
-    		redirect_to new_users_path
-  		else
-    		render :new
-  		end
-    end
+  private
 
-    private
-
-    def user_params
-    	params.require(:user).permit(:username,:email,:password_digest)
-    end	
+  def user_params
+     params.require(:user).permit(:username,:email,:password_digest)
+  end	
 end
